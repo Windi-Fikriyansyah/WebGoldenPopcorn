248b6a341021a9648543aa9d8170b50c959b3f1a
windi <diwin321@gmail.com>
2024-12-16 16:03:57 +0700

    coba

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/user/Login';
 import RegisterPage from './pages/user/Register';
 import HomePage from './pages/user/Home';
